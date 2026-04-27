daf013b9ace64eb9f09280570187d82c3e0733d5
gmanyani527 <131324621+gmanyani527@users.noreply.github.com>
2026-04-26 21:45:55 -0400

    Add: longest-repeating-substring-with-replacement - submission-5

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-5.py
@@ -0,0 +1,26 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        window = {}
+        left = 0
+        res = 0
+        maxF = 0
+        
+        for right in range(len(s)):
+            char = s[right]
+            if char in window:
+                window[char] += 1
+            else:
+                window[char] = 1
+            
+            if window[char] > maxF: 
+                maxF = window[char]
+            while (right - left + 1) - maxF > k:
+                left_char = s[left]
+                window[left_char] -= 1
+                left += 1
+            window_size = right - left + 1
+            if window_size > res: 
+                res = window_size
+        return res
+
+
