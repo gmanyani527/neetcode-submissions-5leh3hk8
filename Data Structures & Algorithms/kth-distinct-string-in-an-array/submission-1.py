863fe3a2e8c3c183e631c8dafa966591a09af048
gmanyani527 <131324621+gmanyani527@users.noreply.github.com>
2026-06-09 23:19:28 -0400

    Add: kth-distinct-string-in-an-array - submission-1

---
+++ Data Structures & Algorithms/kth-distinct-string-in-an-array/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        freq = {} 
+        for word in arr: 
+            if word in freq: 
+                freq[word] += 1
+            else: 
+                freq[word] = 1
+        for word in arr: 
+            if freq[word] == 1: 
+                k -= 1
+
+                if k == 0:
+                    return word
+        return ""
